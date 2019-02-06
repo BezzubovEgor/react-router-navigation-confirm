7a099361609df4761f737f50a57e9e1b2cfc9deb
bezzubov_egor <bezzubov.egor@gmail.com>
2019-02-06 11:46:55 +0300

    Updates

--- src/__test__/__mocks__/index.ts
+++
@@ -1,3 +0,0 @@
-export * from "./HistoryMock";
-export * from "./LocationMock";
-export * from "./MatchMock";
--- src/lib/index.ts
+++
@@ -1,7 +0,0 @@
-export {
-  NavigationConfirmWithRouter as NavigationConfirm
-} from "./NavigationConfirm";
-export {
-  HistoryListenerWithRouter as HistoryListener
-} from "./HistoryListener";
-export { NavigationConfirmModal } from "./NavigationConfirmModal";
