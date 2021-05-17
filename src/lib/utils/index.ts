1668d6f841276958d9fb39dbeb4ed51828ae8c51
Yahor Bezzubau <bezzubov.egor@gmail.com>
2021-05-17 03:35:03 +0300

    update v

--- src/lib/utils/index.ts
+++ src/lib/utils/index.ts
@@ -2,9 +2,12 @@ function noop() {
   /* empty function */
 }
 
-// tslint:disable-next-line
-function isFunction(fn: object | number | string | boolean | undefined | Function): fn is Function {
-  return typeof fn === 'function';
+function isFunction(
+  // tslint:disable-next-line
+  fn: object | number | string | boolean | undefined | Function
+  // tslint:disable-next-line
+): fn is Function {
+  return typeof fn === "function";
 }
 
 export { noop, isFunction };
