dda1965c16f080357cc77366a9de1ffd2e767017
Yahor Bezzubau <bezzubov.egor@gmail.com>
2024-01-24 17:32:14 +0100

    Update test-ww.js

--- test-ww.js
+++ test-ww.js
@@ -1,3 +1,3 @@
 console.log('Hello from WW')
 
-fetch('https://pokeapi.co/api/v2/pokemon-species/aegislash').then(res => res.json()).then(data => console.log('LOG', JSON.stringify(data)))
+fetch('http://localhost:3000/api/hello').then(res => res.json()).then(data => console.log('LOG', JSON.stringify(data)))
